3325961449a4c11eb9c034d1f867d9a626d13b45
icyhins <icyhins@gmail.com>
2017-07-04 15:16:05 +0800

    Remove Nth Node From End Of List Solution with Queue.(One Pass)

---
+++ src/io/icyhins/medium/RemoveNthNodeFromEndOfList.java
@@ -0,0 +1,81 @@
+package io.icyhins.medium;
+
+import io.icyhins.util.ListNode;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+/**
+ * Given a linked list, remove the nth node from the end of list and return its head.
+ *
+ * For example,
+ *
+ * Given linked list: 1->2->3->4->5, and n = 2.
+ *
+ * After removing the second node from the end, the linked list becomes 1->2->3->5.
+ * Note:
+ * Given n will always be valid.
+ * Try to do this in one pass.
+ */
+public class RemoveNthNodeFromEndOfList {
+
+    private ListNode solution(ListNode head, int n){
+
+        int count = 0;
+
+        Deque<ListNode> queue = new ArrayDeque<>();
+
+        ListNode dummy = new ListNode("0");
+        dummy.next = head;
+        ListNode pointer = dummy;
+        while(pointer!= null){
+            count++;
+            if(count > n + 1){
+                queue.pollFirst();
+                queue.offerLast(pointer);
+            }else{
+                queue.offerLast(pointer);
+            }
+
+            pointer = pointer.next;
+        }
+
+        pointer = queue.getFirst();//L(N-n)
+        if(pointer.next!=null){
+            pointer.next = pointer.next.next;
+        }
+
+        return dummy.next;
+    }
+
+    public static void main(String[] args){
+        RemoveNthNodeFromEndOfList obj = new RemoveNthNodeFromEndOfList();
+
+        ListNode head = new ListNode("1");
+        head.next = new ListNode("2");
+        head.next.next = new ListNode("3");
+        head.next.next.next = new ListNode("4");
+        head.next.next.next.next = new ListNode("5");
+        head.next.next.next.next.next = new ListNode("6");
+        head.next.next.next.next.next.next = new ListNode("7");
+        head.next.next.next.next.next.next.next = new ListNode("8");
+        head.next.next.next.next.next.next.next.next = new ListNode("9");
+
+
+        int n = 1;
+        sysout(head);
+        System.out.println();
+        head = obj.solution(head, n);
+        sysout(head);
+
+    }
+
+    private static void sysout(ListNode head){
+        while(head != null){
+            System.out.print(head);
+            System.out.print(" --> ");
+            head = head.next;
+
+        }
+    }
+}
