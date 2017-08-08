b0de985ce68fe9c8e6c573cdd36bd044d8f7ff5e
icyhins <icyhins@gmail.com>
2017-08-08 13:28:56 +0800

    Class Interval for problems.

---
+++ src/io/icyhins/util/Interval.java
@@ -0,0 +1,23 @@
+package io.icyhins.util;
+
+/**
+ * Created by icyhins on 2017/8/4.
+ */
+public class Interval {
+    public int start;
+    public int end;
+    public Interval() {
+        start = 0;
+        end = 0;
+    }
+    public Interval(int s, int e) {
+        start = s;
+        end = e;
+    }
+    public  int getStart(){
+        return this.start;
+    }
+    public  int getEnd(){
+        return this.end;
+    }
+}
